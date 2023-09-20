9533f2f973001c91359b6ff59c90e1e733638de7
Omar Terrazas <terrazas.omar@gmail.com>
2023-09-19 18:04:20 -0600

    Iniciaización de PWA

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [{
+          urlPattern: ({ url }) => {
+            return url.pathname.startsWith("/api");
+          },
+          handler: "CacheFirst",
+          options: {
+            cacheName: "api-cache",
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+          }
+        }]
+      }
+    })
+  ],
+  build: {
+    outDir: "/var/www/html/iagenedu/",
+    emptyOutDir: true
+  }
+});
