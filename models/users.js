e2f08d7d86e691241bd5d1a2f9b1fcfe96e518c9
Omar Terrazas <terrazas.omar@gmail.com>
2023-09-21 16:00:26 -0600

    Integración de funcionalidad para la validación de la cuenta

--- models/users.js
+++ models/users.js
@@ -1,7 +1,41 @@
+const nodemailer = require("nodemailer"),
+  mailConfig = require("../config/mailer");
+
 module.exports = (sequelize, type) => {
-  return sequelize.define("users", {
+  const Users = sequelize.define("users", {
     email: type.STRING,
     password: type.STRING,
-    active: type.BOOLEAN
+    active: {
+      type: type.BOOLEAN,
+      defaultValue: false,
+    },
+    hash: type.STRING,
   });
+
+  Users.prototype.sendWelcomeEmail = async function () {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: mailConfig.user,
+        pass: mailConfig.password,
+      },
+    });
+
+    const mailOptions = {
+      from: '"Grupo de trabajo sobre inteligencia artificial generativa (IAGen) de la UNAM" <iagenedu@unam.mx>',
+      to: this.email,
+      subject: "✅ Comencemos validando tu cuenta",
+      text: `Te damos la bienvenida al sitio web de la I JORNADA DE IA GENERATIVA EN EDUCACIÓN, UNAM 2023. Para comenzar a utilizar tu cuenta, por favor valida tu correo electrónico a través del siguiente enlace: https://iagen.cuaieed.unam.mx/validate/${this.hash}`,
+      html: `<div style="font-size: 24px"><p>Te damos la bienvenida al sitio web de la <strong>I Jornada de IA Generativa en Educación, UNAM 2023</strong>.</p><p>Para comenzar a utilizar tu cuenta, por favor valida tu correo electrónico a través del siguiente <a href="http://localhost:6600/user/validate/${this.hash}">enlace</a>.</p></div>`,
+    };
+
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Message sent: %s", info.messageId);
+    } catch (error) {
+      console.log("Error to send email messages", error);
+    }
+  };
+
+  return Users;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express"),
   db = require("../sequelize"),
-  app = express();
+  app = express(),
+  crypto = require('crypto'),
+  secret = "iagenedu";
 
 module.exports = (app) => {
   app.route("/users").get(function (req, res) {
@@ -8,30 +10,50 @@ module.exports = (app) => {
   });
 
   app.route("/users/:id").get(function (req, res) {
-    db.users.findAll({
-      where: {
-        id: req.params.id
-      },
-      include: [{
-        model: db.sigecos
-      }]
-    }).then((user) => res.json(user));
+    db.users
+      .findAll({
+        where: {
+          id: req.params.id,
+        },
+        include: [
+          {
+            model: db.sigecos,
+          },
+        ],
+      })
+      .then((user) => res.json(user));
   });
 
   app.route("/users").post(function (req, res) {
-    db.users.create(req.body).then((user) => {
-      db.sigecos
-        .create({
-          userId: user.id,
-          name: req.body.name,
-          lastname: req.body.lastname,
-          entity: req.body.entity,
-          account: req.body.account,
-          curp: req.body.curp,
-          studyLevel: req.body.studyLevel,
-        })
-        .then((sigeco) => res.json(user));
-    });
+    const hash = crypto.createHash("sha256", secret)
+      .update(req.body.email+req.body.password)
+      .digest("hex");
+    const password = crypto.createHash("sha256", secret)
+      .update(req.body.password)
+      .digest("hex");
+    db.users
+      .create({
+        email: req.body.email,
+        password: password,
+        hash: hash,
+      })
+      .then((user) => {
+        db.sigecos
+          .create({
+            userId: user.id,
+            name: req.body.name,
+            lastname: req.body.lastname,
+            entity: req.body.entity,
+            account: req.body.account,
+            curp: req.body.curp,
+            studyLevel: req.body.studyLevel,
+          })
+          .then((sigeco) => {
+            user.sendWelcomeEmail().then(() => {
+              res.json(user);
+            });
+          });
+      });
   });
 
   app.route("/users/:id").put(function (req, res) {
@@ -52,5 +74,24 @@ module.exports = (app) => {
         },
       })
       .then((sigeco) => res.json(sigeco));
-  })
+  });
+
+  app.route("/user/validate/:hash").get(function (req, res) {
+    db.users
+      .update(
+        { active: true },
+        {
+          where: {
+            hash: req.params.hash,
+          },
+        }
+      )
+      .then((user) => {
+        if (user) {
+          res.redirect("http://iagenedu.unam.mx/");
+        } else {
+          res.redirect("https://bunam.unam.mx/");
+        }
+      });
+  });
 };
