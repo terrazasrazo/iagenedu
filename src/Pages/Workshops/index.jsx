56c346e005030fd25c872a3c88da579c2cc19c58
Omar Terrazas <terrazas.omar@gmail.com>
2023-09-22 02:01:42 -0600

    Se agrega leyenda de niveles para Workshops

--- src/Pages/Workshops/index.jsx
+++ src/Pages/Workshops/index.jsx
@@ -1,25 +1,28 @@
-import { useState, useEffect } from 'react'
-import Card from '../../Components/Card'
+import { useState, useEffect } from "react";
+import Card from "../../Components/Card";
+
 function Workshops() {
-  const [items, setItems] = useState(null)
+  const [items, setItems] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:6600/workshopassistants')
-      .then(response => response.json())
-      .then(data => setItems(data))
-  }, [])
+    fetch("http://localhost:6600/workshopassistants")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
     <>
-      <div className='workshop-levels bg-white p-4 shadow-sm rounded'>
-        <p>Nivel de conocimiento de IAGen que se requiere para el taller: <span className='bg-blue-400'>Elemental</span> <span className='bg-blue-600'>Intermedio</span> <span className='bg-blue-900'>Avanzado</span></p>
+      <div className="workshop-levels bg-white p-4 shadow-sm rounded">
+        <p>
+          Nivel de conocimiento de IAGen que se requiere para el taller:{" "}
+          <span className="bg-blue-400">Elemental</span>{" "}
+          <span className="bg-blue-600">Intermedio</span>{" "}
+          <span className="bg-blue-900">Avanzado</span>
+        </p>
       </div>
       <section className="container grid grid-cols-4 mx-auto">
-        {
-          
-          items?.map(item => {
-            return <Card key={item.id} data={item} />
-          })
-        }
+        {items?.map((item) => {
+          return <Card key={item.id} data={item} />;
+        })}
       </section>
     </>
   );
