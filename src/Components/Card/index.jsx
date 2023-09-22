499c871ba9b242b48cdc352f10d361b4ad530259
Omar Terrazas <terrazas.omar@gmail.com>
2023-09-21 20:19:12 -0600

    Se le colocan clases a cards por nivel

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom";
+import './Card.css'
 
 const Card = (data) => {
+  let level = ''
+  if(data.data.level === 1) level = 'basic'
+  if(data.data.level === 2) level = 'intermediate'
+  if(data.data.level === 3) level = 'advanced'
   return (
-    <article className="m-6 shadow-sm bg-gray-50 rounded border-t-4 border-blue-500 relative pb-20">
+    <article className={`${level} m-6 shadow-sm bg-gray-50 rounded border-t-4 relative pb-20`}>
       <NavLink to={`/iagenedu/workshops/${data.data.id}`}>
-        <header className="p-4 border-b border-blue-500">
-          <h1 className="text-2xl mb-4 text-blue-500">{data.data.title}</h1>
+        <header className="p-4 border-b">
+          <h1 className="text-2xl mb-4">{data.data.title}</h1>
           {
             data.data.speakers?.map(speaker => {
               return <h4 className="text-right text-lg text-gray-800" key={speaker.id}>{speaker.name}</h4>
@@ -15,7 +20,7 @@ const Card = (data) => {
         <main className="p-2 py-4 text-gray-700">
           <p>{data.data.purpouse}</p>
         </main>
-        <footer className="bg-blue-500 text-gray-100 p-2 rounded-b absolute bottom-0 w-full">
+        <footer className="text-gray-100 p-2 rounded-b absolute bottom-0 w-full">
           <p className="text-xs">{data.data.keywords}</p>
         </footer>
       </NavLink>
