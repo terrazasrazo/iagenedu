73badde53c943c269f0e7d83e5eb99a71d395d67
Omar Terrazas <terrazas.omar@gmail.com>
2023-10-16 18:37:16 -0600

    Responsive design para Workshops

--- src/Pages/Workshops/index.jsx
+++ src/Pages/Workshops/index.jsx
@@ -12,12 +12,8 @@ function Workshops() {
   return (
     <>
       <div className="workshop-levels bg-white p-4 shadow-sm rounded">
-        <p>
-          Nivel de conocimiento de IAGen que se requiere para el taller:{" "}
-          <span className="bg-blue-400 ml-2">Elemental</span>{" "}
-          <span className="bg-blue-600 ml-2">Intermedio</span>{" "}
-          <span className="bg-blue-900 ml-2">Avanzado</span>
-        </p>
+        <p className="mb-4">Nivel de conocimiento de IAGen que se requiere para el taller:</p>
+        <p><span className="bg-blue-400 ml-2">Elemental</span><span className="bg-blue-600 ml-2">Intermedio</span><span className="bg-blue-900 ml-2">Avanzado</span></p>
         <p className="mt-6 text-orange-800">Los talleres ocurrirán de manera simultánea de 12:00 a 14:00 horas.</p>
         <p className="mt-6 text-sm text-orange-600">* Las grabaciones de los talleres estarán disponibles en esta misma plataforma después del evento.</p>
       </div>
@@ -26,7 +22,7 @@ function Workshops() {
           <img src="/images/iagen-coursera.jpeg" alt="IA generativa en el aula" />
         </a>
       </section>
-      <section className="container grid grid-cols-4 mx-auto">
+      <section className="container sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
         {items?.map((item) => {
           return <Card key={item.id} data={item} />;
         })}
