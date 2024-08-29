12d92bbd710dcdec183394a18bea8561859db541
Omar Terrazas <terrazas.omar@gmail.com>
2024-08-29 14:20:29 -0600

    Actualización de logos de dependencias

--- src/Components/Institutions/InstitutionsBlock.jsx
+++ src/Components/Institutions/InstitutionsBlock.jsx
@@ -4,15 +4,23 @@ import institutionsArray from "./institutions";
 function InstitutionsBlock() {
   return (
     <section className="institutions">
-      {institutionsArray?.map((item, index) => {
-        return (
-          <div key={index} className="institutions__item">
-            <a href={item.url} target="_blank" rel="noreferrer">
-              <img src={item.logo} alt={item.name} />
-            </a>
-          </div>
-        );
-      })}
+      <header className="institutions__header">
+        <h2>
+          Dependencias que conforman el Grupo de Trabajo <br />de Inteligencia
+          Artificial Generativa <br />en Educación de la UNAM
+        </h2>
+      </header>
+      <article className="institutions__content">
+        {institutionsArray?.map((item, index) => {
+          return (
+            <div key={index} className="institutions__content--item">
+              <a href={item.url} target="_blank" rel="noreferrer">
+                <img src={item.logo} alt={item.name} />
+              </a>
+            </div>
+          );
+        })}
+      </article>
     </section>
   );
 }
