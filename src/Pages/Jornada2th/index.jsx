ac17fe49180f14b00a6e68d0953171eda9a0ae1a
Omar Terrazas <terrazas.omar@gmail.com>
2024-08-29 14:22:24 -0600

    Se agrega espacio para programa de 2a Jornada

---
+++ src/Pages/Jornada2th/index.jsx
@@ -0,0 +1,16 @@
+import "./Jornada2th.css";
+
+function Jornada2th() {
+  return (
+    <>
+      <section className="jornada-2">
+      <img src="images/jornada2th.jpg" alt="2a Jornada" />
+    </section>
+    <section className="schedule">
+      <h3 className="text-center text-4xl my-40">Próximamente el programa completo</h3>
+    </section>
+    </>
+  );
+}
+
+export default Jornada2th;
