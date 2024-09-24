c024762d45586e52c537f52832a0148c6c73c151
Omar Terrazas <terrazas.omar@gmail.com>
2024-09-24 17:15:02 -0600

    Se carga página de 2a Jornada

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,4 @@
-import { useRoutes, BrowserRouter } from "react-router-dom";
+import { useRoutes, BrowserRouter, Route, Routes } from "react-router-dom";
 import { KeywordProvider } from "../../Context";
 
 import HeaderBlock from "../../Components/HeaderBlock/HeaderBlock";
@@ -6,28 +6,18 @@ import FooterBlock from "../../Components/FooterBlock/FooterBlock";
 
 import Home from "../Home";
 import Jornada2th from "../Jornada2th";
-import CallFor from "../CallFor";
 import InterestingResources from "../InterestingResources";
 import Findings from "../Findings";
-import Register from "../Register";
-import UserProfile from "../UserProfile";
-import UserActivate from "../UserActivate";
-import PasswordRecovery from "../PasswordRecovery";
+import User from "../User/User";
 import NotFound from "../NotFound";
-import LoginForm from "../../Components/LoginForm";
 import Keywords from "../Keywords";
 
 const AppRoutes = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/2ajornada", element: <Jornada2th /> },
-    { path: "/call-for", element: <CallFor /> },
+    { path: "/2a-jornada/:jornadaState?", element: <Jornada2th /> },
     { path: "/resources", element: <InterestingResources /> },
     { path: "/recomendaciones", element: <Findings /> },
-    { path: "/register", element: <Register /> },
-    { path: "/profile", element: <UserProfile /> },
-    { path: "/activate/:userHash", element: <UserActivate /> },
-    { path: "/recovery/:userHash", element: <PasswordRecovery /> },
     { path: "/keywords/", element: <Keywords /> },
     { path: "*", element: <NotFound /> },
   ]);
@@ -37,12 +27,21 @@ function App() {
   return (
     <KeywordProvider>
       <BrowserRouter>
-        <HeaderBlock />
-        <main className="container mx-auto">
-          <AppRoutes />
-        </main>
-        <LoginForm />
-        <FooterBlock />
+        <Routes>
+          <Route path="/user/:userState/:userHash?" element={<User />} />
+          <Route
+            path="*"
+            element={
+              <>
+                <HeaderBlock />
+                <main className="container mx-auto">
+                  <AppRoutes />
+                </main>
+                <FooterBlock />
+              </>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </KeywordProvider>
   );
--- src/Pages/Jornada2th/index.jsx
+++ src/Pages/Jornada2th/index.jsx
@@ -1,15 +1,61 @@
+import { useParams, NavLink } from "react-router-dom";
 import "./Jornada2th.css";
 
 function Jornada2th() {
+  const { jornadaState } = useParams();
+
+  const jornadaStateData = (jornadaState) => {
+    if (jornadaState === "workshops") {
+      return (
+        <article className="jornada2th__workshops">
+          <h3 className="text-center text-4xl my-40">
+            Próximamente los talleres
+          </h3>
+        </article>
+      );
+    } else if (jornadaState === "call-for") {
+      return (
+        <article className="jornada2th__callfor">
+          <img src="/images/jornada2th/convocatoria.jpg" alt="Convocatoria" />
+        </article>
+      );
+    } else {
+      return (
+        <article className="jornada2th__schedule">
+          <h3 className="text-center text-4xl my-40">
+            Próximamente el programa completo
+          </h3>
+        </article>
+      );
+    }
+  };
+
   return (
-    <>
-      <section className="jornada-2">
-      <img src="images/jornada2th.jpg" alt="2a Jornada" />
+    <section className="jornada2th">
+      <header className="jornada2th__header">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/2a-jornada">Programa</NavLink>
+            </li>
+            <li>
+              <NavLink to="/2a-jornada/workshops">Talleres</NavLink>
+            </li>
+            <li>
+              <NavLink to="/2a-jornada/call-for">Convocatoria</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {jornadaState === "call-for" ? (
+        <></>
+      ) : (
+        <article className="jornada2th__savethedate">
+          <img src="/images/jornada2th.jpg" alt="2a Jornada" />
+        </article>
+      )}
+      {jornadaStateData(jornadaState)}
     </section>
-    <section className="schedule">
-      <h3 className="text-center text-4xl my-40">Próximamente el programa completo</h3>
-    </section>
-    </>
   );
 }
 
