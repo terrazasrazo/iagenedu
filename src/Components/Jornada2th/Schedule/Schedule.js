eb09aba5c903f1e6000849d7b47a69b70a16b7bc
Omar Terrazas <terrazas.omar@gmail.com>
2024-10-04 17:23:26 -0600

    Actualización del programa

---
+++ src/Components/Jornada2th/Schedule/Schedule.js
@@ -0,0 +1,33 @@
+const scheduleArray = [
+    {
+        date: "14 de noviembre de 2024",
+        items: [
+            { time: "09:00", event: "Bienvenida e inauguración" },
+            { time: "09:30", event: "Conferencia IAGen y educación" },
+            { time: "10:30", event: "Mesa \"Nuevos roles docentes en la era de la IAGen\"" },
+            { time: "11:30", event: "<em>Receso</em>" },
+            { time: "11:40", event: "<a href=\"/2a-jornada/workshops/58\" target=\"_blank\" class=\"font-semibold underline\">Promptatón</a> y Talleres" },
+            { time: "13:40", event: "<em>Receso para comida</em>" },
+            { time: "15:00", event: "Presentaciones relámpago" },
+            { time: "17:00", event: "Mesa \"Explorar el uso de la IAGen: voz del estudiantado\"" },
+            { time: "18:00", event: "Cierre día 1" }
+        ]
+    },
+    {
+        date: "15 de noviembre de 2024",
+        items: [
+            { time: "09:00", event: "Mesa \"Aplicaciones educativas de IA Gen: retos éticos\"" },
+            { time: "10:00", event: "Conferencia \"Rediseñar la evaluación con IAGen: ¿cuál camino tomar?\"" },
+            { time: "11:00", event: "<em>Receso</em>" },
+            { time: "11:10", event: "<a href=\"/2a-jornada/workshops/59\" target=\"_blank\" class=\"font-semibold underline\">Nanohakatón</a> y Talleres" },
+            { time: "13:10", event: "Mesa \"Futuros de IAGen en educación en la UNAM\"" },
+            { time: "14:00", event: "<em>Receso para comida</em>" },
+            { time: "15:00", event: "Presentaciones relámpago" },
+            { time: "16:00", event: "Mejores propuestas del certamen" },
+            { time: "16:30", event: "Premiación y clausura" },
+            { time: "16:45", event: "Cierre día 2" },
+        ]
+    },    
+];
+
+export default scheduleArray;
