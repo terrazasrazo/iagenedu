ea884d08cfeec59af4cdf6cf08ad8daf14dc02d2
Omar Terrazas <terrazas.omar@gmail.com>
2024-11-04 11:20:50 -0600

    Se integra etiqueta de "Lugares agotados"

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,18 +1,18 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { KeywordContext } from "../../Context";
-import './Card.css'
+import "./Card.css";
 
 const Card = (data) => {
   const context = useContext(KeywordContext);
 
-  let level = ''
-  if(data.data.level === 1) level = 'basic'
-  if(data.data.level === 2) level = 'intermediate'
-  if(data.data.level === 3) level = 'advanced'
+  let level = "";
+  if (data.data.level === 1) level = "basic";
+  if (data.data.level === 2) level = "intermediate";
+  if (data.data.level === 3) level = "advanced";
 
-  let availableSeats = data.data.participants - data.data.assistantsCount
-  if(availableSeats < 0) availableSeats = 0
+  let availableSeats = data.data.participants - data.data.assistantsCount;
+  if (availableSeats < 0) availableSeats = 0;
 
   const keywordsArray = data.data.keywords.split(",");
 
@@ -23,21 +23,24 @@ const Card = (data) => {
 
   return (
     <article className={`${level} workshop`}>
-       
-        <header className="workshop__header">
-          <h3 className="workshop__header--title">{data.data.title}</h3>
-          {
-            data.data.speakers?.map(speaker => {
-              return <h4 className="workshop__header--speaker" key={speaker.id}>{speaker.name}</h4>
-            })
-          }
-        </header>
-        <main className="workshop__details">
-          <p className="workshop__details--ocurrence"><span>{data.data.ocurrenceDay}</span></p>
-          {workshopContent(data.data.purpouse)}
-          <p className="workshop__details--link"><NavLink to={`/2a-jornada/workshops/${data.data.id}`} >ver detalles del taller</NavLink></p>
-        </main>
-        <footer className="workshop__tags">
+      {data.data.assistantsCount >= 70 ? (
+        <span className="workshop__details--seats full">Lugares agotados</span>
+      ):(<span className="workshop__details--seats"></span>)}
+      <header className="workshop__header">
+        <h3 className="workshop__header--title">{data.data.title}</h3>
+      </header>
+      <main className="workshop__details">
+        <p className="workshop__details--ocurrence">
+          <span>{data.data.ocurrenceDay}</span>
+        </p>
+        {workshopContent(data.data.purpouse)}
+        <p className="workshop__details--link">
+          <NavLink to={`/2a-jornada/workshops/${data.data.id}`}>
+            ver detalles del taller
+          </NavLink>
+        </p>
+      </main>
+      <footer className="workshop__tags">
         {keywordsArray.map((keyword, index) => {
           return (
             <NavLink
@@ -46,9 +49,7 @@ const Card = (data) => {
               key={index}
               onClick={() => context.setKeyword(keyword)}
             >
-              <span>
-                {keyword}
-              </span>
+              <span>{keyword}</span>
             </NavLink>
           );
         })}
