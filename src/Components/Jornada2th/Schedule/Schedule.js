e1ae5d3582c90ac22913b9bb5f901de3fbfeccee
Omar Terrazas <terrazas.omar@gmail.com>
2025-01-10 14:01:05 -0600

    Definición de estructura del sitio

--- src/Components/Jornada2th/Schedule/Schedule.js
+++ src/Components/Jornada2th/Schedule/Schedule.js
@@ -1,32 +1,400 @@
 const scheduleArray = [
-    {
-        date: "14 de noviembre de 2024",
-        items: [
-            { time: "09:00", event: "Bienvenida e inauguración", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "09:30", event: "Conferencia \"Artificial Intelligence & The Future of Education\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "10:30", event: "Mesa \"Nuevos roles docentes en la era de la IAGen\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "11:30", event: "<em>Receso</em>", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "11:40", event: "<a href=\"/2a-jornada/workshops/58\" target=\"_blank\" class=\"font-semibold underline\">Promptatón</a> y Talleres", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "13:40", event: "<em>Receso para comida</em>", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "15:00", event: "Presentaciones relámpago", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "17:00", event: "Mesa \"Explorar el uso de la IAGen: voz del estudiantado\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "18:00", event: "Cierre día 1", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" }
-        ]
-    },
-    {
-        date: "15 de noviembre de 2024",
-        items: [
-            { time: "09:00", event: "Mesa \"Aplicaciones educativas de IA Gen: retos éticos\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "10:00", event: "Conferencia \"Rediseñar la evaluación con IAGen: ¿cuál camino tomar?\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "11:00", event: "<em>Receso</em>", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "11:10", event: "<a href=\"/2a-jornada/workshops/59\" target=\"_blank\" class=\"font-semibold underline\">Nanohakatón</a> y Talleres", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "13:10", event: "Mesa \"Futuros de IAGen en educación en la UNAM\"", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "14:00", event: "<em>Receso para comida</em>", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "15:00", event: "Presentaciones relámpago", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "16:45", event: "Premiación y clausura", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" },
-            { time: "17:00", event: "Cierre día 2", "link": "https://unam.zoom.us/webinar/register/WN__SgTPRZ9Q720zfHQdjwkpQ" }
-        ]
-    },    
+  {
+    date: "14 de noviembre de 2024",
+    items: [
+      {
+        event: "Bienvenida e inauguración",
+        link: "bienvenida-inauguracion",
+        video: "R8FhqzQ_aYE",
+        actors: [
+          {
+            name: "Dra. Patricia Dávila Aranda",
+            institution: "Secretaría General",
+            links: [
+              { social: "x", link: "https://x.com/patriciaddavila" },
+              { social: "web", link: "https://www.patriciadavila.mx/" },
+              {
+                social: "scholar",
+                link: "https://scholar.google.com.mx/citations?user=JKueST8AAAAJ",
+              },
+            ],
+          },
+          {
+            name: "Dr. Melchor Sánchez Mendiola",
+            institution: "CEIDE",
+            links: [
+              { social: "x", link: "https://twitter.com/melchorsm" },
+              {
+                social: "scholar",
+                link: "https://scholar.google.com/citations?user=2l-cMhYAAAAJ&hl=es",
+              },
+            ],
+          },
+          { name: "Dra. Anabel de la Rosa Gómez", institution: "CUAED" },
+          { name: "Dr. Boris Escalante Ramírez", institution: "CECAV" },
+          { name: "Dr. Hector Benítez Pérez", institution: "DGTIC" },
+          { name: "Dra. Elsa Margarita Ramírez Leyva", institution: "DGB" },
+        ],
+      },
+      {
+        event:
+          'Conferencia "Artificial Intelligence & The Future of Education"',
+        link: "ai-future-education",
+        video: "IVYwXs_83Lw",
+        actors: [
+          {
+            name: "Dr. David Dixon",
+            institution: "MIT",
+            links: [
+              { social: "x", link: "https://x.com/rdavedxn" },
+              {
+                social: "linkedin",
+                link: "https://www.linkedin.com/in/rdaviddixon/",
+              },
+            ],
+          },
+          {
+            name: "Dr. Melchor Sánchez Mendiola",
+            institution: "CEIDE",
+            links: [
+              { social: "x", link: "https://twitter.com/melchorsm" },
+              {
+                social: "scholar",
+                link: "https://scholar.google.com/citations?user=2l-cMhYAAAAJ&hl=es",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        event: 'Mesa "Nuevos roles docentes en la era de la IAGen"',
+        link: "nuevos-roles-docentes",
+        video: "geNbK0XMjnA",
+        actors: [
+          {
+            name: "Dra. Jimena Olveres",
+            institution: "CECAV",
+            links: [],
+          },
+          {
+            name: "Dr. Víctor Jesús Rendón Cazales",
+            institution: "CEIDE",
+            links: [],
+          },
+          {
+            name: "Dr. Gustavo de la Cruz",
+            institution: "ICAT",
+            links: [],
+          },
+          {
+            name: "Dra. Anabel de la Rosa",
+            institution: "CUAED",
+            links: [],
+          },
+          {
+            name: "Dra. Marina Kriscautzky",
+            institution: "DGTIC",
+            links: [],
+          },
+        ],
+      },
+      {
+        event: "Presentaciones relámpago",
+        link: "presentaciones-relampago-14nov",
+        video: "5KF-OgEdOCQ",
+        actors: [
+          {
+            name: "María del Carmen González Videgaray",
+            institution: "FES Acatlán",
+            links: [],
+          },
+          {
+            name: "Monserrat de Jesus Guerra Ocampo",
+            institution: "ENALT",
+            links: [],
+          },
+          {
+            name: "Gabriel Cuevas Nuñez",
+            institution: "ENP 5 José Vasconcelos",
+            links: [],
+          },
+          {
+            name: "Ivonne Guadalupe Rocha Díaz",
+            institution: "Facultad de Artes y Diseño",
+            links: [],
+          },
+          {
+            name: "Mario Alberto Valle Zavala",
+            institution: "Escuela Nacional Preparatoria",
+            links: [],
+          },
+          {
+            name: "Jahaziel Quintín Cruz García",
+            institution: "Facultad de Medicina",
+            links: [],
+          },
+          {
+            name: "Adriana Gaytán Caballero",
+            institution: "Facultad de Ciencias",
+            links: [],
+          },
+          {
+            name: "Vanessa Sattele",
+            institution: "Centro de Investigaciones de Diseño Industrial",
+            links: [],
+          },
+          {
+            name: "Esther Mahuina Campos Castolo",
+            institution: "Facultad de Medicina",
+            links: [],
+          },
+          {
+            name: "Ivonne Estefania Zepeda Gutiérrez",
+            institution: "Unidad de posgrado, Facultad de Medicina",
+            links: [],
+          },
+          {
+            name: "Carlos Nandayapa Hernández",
+            institution: "FES Acatlán",
+            links: [],
+          },
+          {
+            name: "Armando Cervantes Sandoval",
+            institution: "FES Zaragoza",
+            links: [],
+          },
+          {
+            name: "Dra. Ana Yuri Ramírez",
+            institution: "DGTIC",
+            links: [],
+          },
+          {
+            name: "Mtro. Alan Hernández",
+            institution: "CEIDE",
+            links: [],
+          },
+        ],
+      },
+      {
+        event: 'Mesa "Explorar el uso de la IAGen: voz del estudiantado"',
+        link: "explorar-uso-iagen",
+        video: "XUP-ZvKXz9I",
+        actors: [
+          {
+            name: "Rodrigo Cardoso",
+            institution: "Facultad de Ciencias Políticas y Sociales",
+            links: [],
+          },
+          {
+            name: "Yoatzi Álvarez Galván",
+            institution: "Facultad de Medicina",
+            links: [],
+          },
+          {
+            name: "Alejandra Castillo Pedraza",
+            institution: "Facultad de Arte y Diseño",
+            links: [],
+          },
+          {
+            name: "Emma Valentina Rueda Sánchez Méndez",
+            institution: "Escuela Nacional Preparatoria",
+            links: [],
+          },
+          {
+            name: "Benito Vicente Franco López",
+            institution: "Facultad de Ciencias",
+            links: [],
+          },
+          {
+            name: "Mtra. Verónica Luna",
+            institution: "CEIDE",
+            links: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    date: "15 de noviembre de 2024",
+    items: [
+      {
+        event: 'Mesa "Aplicaciones educativas de IA Gen: retos éticos"',
+        link: "iagen-retos-eticos",
+        video: "XcE24bwZztE",
+        actors: [
+          {
+            name: "Jorge Enrique Linares Salgado",
+            institution: "FFyL",
+            links: [],
+          },
+          {
+            name: "Dra Jennifer Hincapié Sánchez",
+            institution: "PUB",
+            links: [],
+          },
+          {
+            name: "Dra. Jacqueline Bucio",
+            institution: "CUAED",
+            links: [],
+          },
+          {
+            name: "Dr. Carlos Gutierrez Aguilar",
+            institution: "FMVZ",
+            links: [],
+          },
+          {
+            name: "Mtra. Alejandra Cruz Cruz",
+            institution: "CEIDE",
+            links: [],
+          },
+        ],
+      },
+      {
+        event:
+          'Conferencia "Rediseñar la evaluación con IAGen: ¿cuál camino tomar?"',
+        link: "evaluacion-iagen",
+        video: "1L4IRb2rwWE",
+        actors: [
+          {
+            name: "Dr. Melchor Sánchez Mendiola",
+            institution: "CEIDE",
+            links: [],
+          },
+          {
+            name: "Mtra. Verónica Luna",
+            institution: "CEIDE",
+            links: [],
+          },
+        ],
+      },
+      {
+        event: 'Mesa "Futuros de IAGen en educación en la UNAM"',
+        link: "futuros-iagen-educacion",
+        video: "mdHn2Kxtux8",
+        actors: [
+          {
+            name: "Dr Everardo Bárcenas",
+            institution: "FI",
+            links: [],
+          },
+          {
+            name: "Dr Boris Escalante",
+            institution: "CECAV",
+            links: [],
+          },
+          {
+            name: "Dra Guadalupe Vadillo",
+            institution: "CUAED",
+            links: [],
+          },
+          {
+            name: "Dr. Héctor Benítez",
+            institution: "DGTIC",
+            links: [],
+          },
+          {
+            name: "Dr Xavier Soberón",
+            institution: "C3",
+            links: [],
+          },
+        ],
+      },
+      {
+        event: "Presentaciones relámpago",
+        link: "presentaciones-relampago-15nov",
+        video: "jWiTfmsD-5U",
+        actors: [
+          {
+            name: "Ulises Hernández García",
+            institution: "Facultad de Medicina",
+            links: [],
+          },
+          {
+            name: "Amparo Del Carmen Ramírez Fuentes",
+            institution: "ENP 2 Erasmo Castellanos Quinto",
+            links: [],
+          },
+          {
+            name: "Marina Rivero Trejo",
+            institution: "CCH Naucalpan",
+            links: [],
+          },
+          {
+            name: "Vania Jocelyn Pineda Ortega",
+            institution: "CUAED",
+            links: [],
+          },
+          {
+            name: "Humberto Islas",
+            institution: "FES Aragón",
+            links: [],
+          },
+          {
+            name: "Silvia Guadalupe Canabal Cáceres",
+            institution: "ENP 6 Antonio Caso",
+            links: [],
+          },
+          {
+            name: "Jorge Santiago Jacinto",
+            institution: "CEIDE",
+            links: [],
+          },
+          {
+            name: "Rogelio Aurelio Rojas Reyes",
+            institution: "CCH Naucalpan",
+            links: [],
+          },
+          {
+            name: "Jesús Aelxis Aguilar Rodríguez",
+            institution: "FES Zaragoza",
+            links: [],
+          },
+          {
+            name: "Gabriel Alejandro May Lozano",
+            institution: "PCIC UNAM",
+            links: [],
+          },
+          {
+            name: "Dra. Ana Yuri Ramírez",
+            institution: "DGTIC",
+            links: [],
+          },
+          {
+            name: "Mtro. Alan Hernández",
+            institution: "CEIDE",
+            links: [],
+          },
+        ],
+      },
+      {
+        event: "Premiación y clausura",
+        link: "clausura",
+        video: "8H5YQN-oKII",
+        actors: [
+          {
+            name: "Dra. Anabel de la Rosa Gómez",
+            institution: "CUAED",
+            links: [],
+          },
+          {
+            name: "Dr. Melchor Sánchez Mendiola",
+            institution: "CEIDE",
+            links: [],
+          },
+          {
+            name: "Dr. Boris Escalante Ramírez",
+            institution: "CECAV",
+            links: [],
+          },
+          {
+            name: "Dr. Hector Benítez Pérez",
+            institution: "DGTIC",
+            links: [],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default scheduleArray;
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,49 +1,42 @@
 import { useRoutes, BrowserRouter, Route, Routes } from "react-router-dom";
-import { KeywordProvider } from "../../Context";
 
 import HeaderBlock from "../../Components/HeaderBlock/HeaderBlock";
 import FooterBlock from "../../Components/FooterBlock/FooterBlock";
 
-import Home from "../Home";
 import Jornada2th from "../Jornada2th";
-import InterestingResources from "../InterestingResources/InterestingResources";
-import Findings from "../Findings";
-import User from "../User/User";
+import Conference from "../Conference";
+import Workshops from "../Workshops";
+import Workshop from "../Workshop/Workshop";
 import NotFound from "../NotFound";
-import Keywords from "../Keywords";
 
 const AppRoutes = () => {
   return useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/2a-jornada/:jornadaState?/:jornadaDetails?", element: <Jornada2th /> },
-    { path: "/resources", element: <InterestingResources /> },
-    { path: "/recomendaciones", element: <Findings /> },
-    { path: "/keywords/", element: <Keywords /> },
+    { path: "/2a-jornada/", element: <Jornada2th /> },
+    { path: "/2a-jornada/conference/:conferenceName?", element: <Conference /> },
+    { path: "/2a-jornada/workshops", element: <Workshops /> },
+    { path: "/2a-jornada/workshop/:workshopId", element: <Workshop /> },
     { path: "*", element: <NotFound /> },
   ]);
 };
 
 function App() {
   return (
-    <KeywordProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/user/:userState/:userHash?" element={<User />} />
-          <Route
-            path="*"
-            element={
-              <>
-                <HeaderBlock />
-                <main className="container mx-auto">
-                  <AppRoutes />
-                </main>
-                <FooterBlock />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </KeywordProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <>
+              <HeaderBlock />
+              <main className="container mx-auto">
+                <AppRoutes />
+              </main>
+              <FooterBlock />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
