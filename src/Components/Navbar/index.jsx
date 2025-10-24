789c70d73577161f64d03185fc7ab6bc2f58bc12
Omar Terrazas <terrazas.omar@gmail.com>
2025-10-24 17:30:51 -0600

    Elimina enlaces a Recursos y Recomendaciones en la barra de navegación

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -14,12 +14,6 @@ const Navbar = () => {
         <li>
           <NavLink to="/workshops/">Talleres</NavLink>
         </li>
-        <li>
-          <a href={`${import.meta.env.VITE_HOME_URL}/resources/`}>Recursos</a>
-        </li>
-        <li>
-          <a href={`${import.meta.env.VITE_HOME_URL}/recomendaciones/`} className="text-orange_unam font-semibold">Recomendaciones</a>
-        </li>
       </ul>
     </nav>
   );
